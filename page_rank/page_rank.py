2b26a84289f651b7d4b3b408da5fd370d8b4bb58
VulcanWM <logmedha09@gmail.com>
2026-01-28 16:13:29 +0000

    removed first_num and last_num

--- page_rank/page_rank.py
+++ page_rank/page_rank.py
@@ -1,13 +1,13 @@
 import random
 
-def page_rank(outbound_links, first_num, last_num, damping_factor=0.85, n=10000):
-    current_num = random.randint(first_num, last_num)
+def page_rank(outbound_links, damping_factor=0.85, n=10000):
+    current_num = random.choice(list(outbound_links.keys()))
     count = {current_num: 1}
 
     for i in range(n):
         damp = random.randint(1,100)
         if damp > damping_factor * 100:
-            current_num = random.randint(first_num, last_num)
+            current_num = random.choice(list(outbound_links.keys()))
             count[current_num] = count.get(current_num, 0) + 1
         else:
             current_num = random.choice(outbound_links[current_num])
@@ -15,3 +15,5 @@ def page_rank(outbound_links, first_num, last_num, damping_factor=0.85, n=10000)
     for key in count.keys():
         count[key] = round(count[key] / n * 100, 3)
     return count
+
+print(page_rank({1: [2, 3], 2: [3], 3: [1]}, damping_factor=0.85, n=1000000))
